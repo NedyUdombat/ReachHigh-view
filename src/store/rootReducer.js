41c9f2fb8bbb43262b010bb80774bcf72559f3a7
nedyudombat <nedyudombat@gmail.com>
2019-10-20 23:51:26 +0100

    [feature #169248856]User can select goals

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 
 // reducers
 import { authReducer } from './modules/auth';
+import { goalReducer } from './modules/goal';
 
 export default combineReducers({
   auth: authReducer,
+  goal: goalReducer,
 });
